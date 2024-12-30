1197d12bee1ce47141972bebdafb362fbbe397a1
DucKhiemPJ <duckhiemmai30@gmail.com>
2024-12-31 00:11:13 +0700

    Update rfidio.sql

--- rfidio.sql
+++ rfidio.sql
@@ -1,31 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.8.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 06, 2020 at 03:47 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `rfidattendance`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
 
 CREATE TABLE `admin` (
   `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -34,18 +11,10 @@ CREATE TABLE `admin` (
   `admin_pwd` LONGTEXT NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `admin`
---
 
 INSERT INTO `admin` (`id`, `admin_name`, `admin_email`, `admin_pwd`) VALUES
 (1, 'Admin', 'admin@gmail.com', '$2y$10$89uX3LBy4mlU/DcBveQ1l.32nSianDP/E1MfUh.Z.6B4Z0ql3y7PK');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `devices`
---
 
 CREATE TABLE `devices` (
   `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -56,11 +25,6 @@ CREATE TABLE `devices` (
   `device_mode` TINYINT(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 CREATE TABLE `pwd_reset` (
   `pwd_reset_id` INT(11) NOT NULL AUTO_INCREMENT,
   `pwd_reset_email` VARCHAR(255) NOT NULL,
@@ -79,7 +43,7 @@ CREATE TABLE `goods` (
   `card_uid` VARCHAR(30) NOT NULL,
   `card_select` TINYINT(1) NOT NULL DEFAULT '0',
   `good_date` DATE NOT NULL,
-  `exp_date` DATE,
+  `exp_date` VARCHAR(10),
   `device_uid` VARCHAR(20) NOT NULL DEFAULT '0',
   `device_dep` VARCHAR(20) NOT NULL DEFAULT '0',
   `add_card` TINYINT(1) NOT NULL DEFAULT '0'
@@ -106,6 +70,3 @@ CREATE TABLE `goods_logs` (
 
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
